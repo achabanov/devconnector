f4f093093e1b6e024c8a62f3f5d489d7e833f477
Andrey Chabanov <andreyc93@hotmail.com>
2018-10-22 23:37:24 -0700

    jwt implemented

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://andrey1:andrey1@ds237713.mlab.com:37713/devconnector"
+  mongoURI: "mongodb://andrey1:andrey1@ds237713.mlab.com:37713/devconnector",
+  secretOrKey: "secret"
 };
